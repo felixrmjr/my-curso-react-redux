279b184902ee4c3c8b07bef42154f5cfee8205f3
Felix Ribeiro Mansor Junior <felix.mansor@superdigital.com.br>
2017-10-02 16:05:14 -0300

    Criação da pasta da aplicação TODO

---
+++ todo-app/backend/src/config/server.js
@@ -0,0 +1,21 @@
+const port = 3003
+
+// Faz o parser do corpo da requisição
+const bodyParser = require('body-parser')
+// Servidor que roda em cima do node
+const express = require('express')
+// Start do express
+const server = express()
+
+// Configurando o bodyParser
+server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.json())
+
+server.listen(port, function(){
+    console.log(`BACKEND is running on port ${port}`)
+})
+
+
+
+
+
