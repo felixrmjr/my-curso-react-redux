a5882e14517781bcb4409693633a6df28348522e
Felix <felix.mansor@superdigital.com.br>
2017-10-04 14:18:42 -0300

    Versionando até a aula 38 de Cors

--- todo-app/backend/src/config/server.js
+++ todo-app/backend/src/config/server.js
@@ -1,21 +1,27 @@
-const port = 3003
+const port = 3003;
 
 // Faz o parser do corpo da requisição
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+
 // Servidor que roda em cima do node
-const express = require('express')
+const express = require('express');
+
 // Start do express
-const server = express()
+const server = express();
+
+// Habilitando o cors
+const allowCors = require('./cors');
 
 // Configurando o bodyParser
-server.use(bodyParser.urlencoded({extended: true}))
-server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({extended: true}));
+server.use(bodyParser.json());
+server.use(allowCors);
 
 server.listen(port, function(){
-    console.log(`BACKEND is running on port ${port}`)
-})
+    console.log(`BACKEND is running on port ${port}`);
+});
 
-module.exports = server
+module.exports = server;
 
 
 
